2b287289c3f4710579fdc11fd73c3bdadfeaf0e9
lmh <lmh315@yonsei.ac.kr>
2017-12-26 00:55:27 +0900

    tap menu

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+def base(request):
+    return render(request, 'blog/base.html', {})
 # Create your views here.
